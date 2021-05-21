34341ea200b762b4171e24ad0878721292e2f7af
geekdesign <schutz.pa@gmail.com>
2021-05-21 23:51:21 +0200

    install-tailwind-Others

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
+
+function Application({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default Application;
